16976be6d172f98211dacc196c18a52382738eb7
Ahmed Amayem <amayem.yorku@gmail.com>
2014-06-19 11:16:34 -0400

    step-7 Indenting every level down

--- tree.sh
+++ tree.sh
@@ -1,19 +1,21 @@
 #!/bin/sh
 
+declare -i indentSize=5
+
 listdir()
 {
 	local currentDir=($(ls $1))
-	local -i lastIndex=$((${#currentDir[*]} - 1)) index
+	local -i lastIndex=$((${#currentDir[*]} - 1)) index indents=$2
 
 	for ((index=0; index<lastIndex; index++))
 	do
-			printf "├─%s\n" ${currentDir[$index]}
+			printf "%.${indents}s├─%s\n" " " ${currentDir[$index]}
 			if [ -d ${currentDir[$index]} ]; then
-				listdir ${currentDir[$index]}
+				listdir ${currentDir[$index]} $(($indents + $indentSize))
 			fi	
 	done
 
-	printf "└─%s\n" ${currentDir[$lastIndex]}
+	printf "%.${indents}s└─%s\n" " " ${currentDir[$lastIndex]}
 }
 
-listdir $PWD
+listdir $PWD 0
