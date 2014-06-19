757f5e43905a5f69cc63ec2bc1cd30031b3b60d6
Ahmed Amayem <amayem.yorku@gmail.com>
2014-06-18 23:11:48 -0400

    step-3.2.2 More efficient way of cutting off the leftover of the final stem

--- tree.sh
+++ tree.sh
@@ -4,11 +4,9 @@ currentDir=($(ls))
 typeset -i lastIndex index
 lastIndex=$((${#currentDir[*]} - 1))
 
-for index in ${!currentDir[*]}
+for ((index=0; index<lastIndex; index++))
 do
-	if [ "$index" -lt "$lastIndex" ]; then
 		printf "├─%s\n" ${currentDir[$index]}
-	else
-		printf "└─%s\n" ${currentDir[$index]}
-	fi
 done
+
+printf "└─%s\n" ${currentDir[$lastIndex]}
