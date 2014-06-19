78fe9c4b232e252068b7223bbde055983bfc1b12
Ahmed Amayem <amayem.yorku@gmail.com>
2014-06-19 11:25:22 -0400

    step-9 Checking Directories and Passing Full Paths

--- tree.sh
+++ tree.sh
@@ -1,21 +1,24 @@
 #!/bin/sh
 
 declare -i indentSize=5
-
+	
 listdir()
 {
+	local currentPath=$1
 	local currentDir=($(ls $1))
 	local -i lastIndex=$((${#currentDir[*]} - 1)) index indents=$2
 
 	for ((index=0; index<lastIndex; index++))
 	do
 			printf "%.${indents}s├─%s\n" "│ " ${currentDir[$index]}
-			if [ -d ${currentDir[$index]} ]; then
-				listdir ${currentDir[$index]} $(($indents + $indentSize))
+			if [ -d "$currentPath/${currentDir[$index]}" ]; then
+				listdir "$currentPath/${currentDir[$index]}" $(($indents + $indentSize))
 			fi	
 	done
 
-	printf "%.${indents}s└─%s\n" "│ " ${currentDir[$lastIndex]}
+	if [ $lastIndex -ge 0 ]; then
+		printf "%.${indents}s└─%s\n" "│ " ${currentDir[$lastIndex]}
+	fi
 }
 
 listdir $PWD 0
